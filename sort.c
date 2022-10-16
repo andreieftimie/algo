8a4b05a93756b62bc319a0fef7f7b9db86032852
Andrei Eftimie <andrei@eftimie.com>
2022-10-16 10:12:46 +0300

    Remove divideEtImpera

--- sort.c
+++ sort.c
@@ -22,37 +22,6 @@ int insertSort(int *arr, int length) {
   return 0;
 }
 
-int divideEtImpoeraSort(int *arr, int length) {
-  if (length <= 1) {
-    return 0;
-  }
-  int mid = length / 2;
-  divideEtImpoeraSort(arr, mid);
-  divideEtImpoeraSort(arr + mid, length - mid);
-  int *temp = (int *)malloc(sizeof(int) * length);
-  int i = 0;
-  int j = mid;
-  int k = 0;
-  while (i < mid && j < length) {
-    if (arr[i] < arr[j]) {
-      temp[k++] = arr[i++];
-    } else {
-      temp[k++] = arr[j++];
-    }
-  }
-  while (i < mid) {
-    temp[k++] = arr[i++];
-  }
-  while (j < length) {
-    temp[k++] = arr[j++];
-  }
-  for (int i = 0; i < length; i++) {
-    arr[i] = temp[i];
-  }
-  free(temp);
-  return 0;
-}
-
 int mergeSort(int *arr, int length) {
   if (length <= 1) {
     return 0;
@@ -130,7 +99,6 @@ int main() {
   int length = sizeof(array)/sizeof(array[0]);
   printArray(array, length);
   // insertSort(array, length);
-  // divideEtImpoeraSort(array, length);
   // mergeSort(array, length);
   heapSort(array, length);
   printArray(array, length);
