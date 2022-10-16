a622fbb384308673e67384b090a9c22d0192e8ec
Andrei Eftimie <andrei@eftimie.com>
2022-10-16 11:18:32 +0300

    Move mergeSort out

--- sort.c
+++ sort.c
@@ -2,37 +2,6 @@
 #include <stdlib.h>
 #include "util.h"
 
-int mergeSort(int *arr, int length) {
-  if (length <= 1) {
-    return 0;
-  }
-  int mid = length / 2;
-  mergeSort(arr, mid);
-  mergeSort(arr + mid, length - mid);
-  int *temp = (int *)malloc(sizeof(int) * length);
-  int i = 0;
-  int j = mid;
-  int k = 0;
-  while (i < mid && j < length) {
-    if (arr[i] < arr[j]) {
-      temp[k++] = arr[i++];
-    } else {
-      temp[k++] = arr[j++];
-    }
-  }
-  while (i < mid) {
-    temp[k++] = arr[i++];
-  }
-  while (j < length) {
-    temp[k++] = arr[j++];
-  }
-  for (int i = 0; i < length; i++) {
-    arr[i] = temp[i];
-  }
-  free(temp);
-  return 0;
-}
-
 int heapSort(int *arr, int length) {
   for (int i = length / 2 - 1; i >= 0; i--) {
     int j = i;
@@ -78,7 +47,6 @@ int main() {
   int array[] = {6, 4, 2, 8, 1, 9, 3, 5, 7, 0};
   int length = sizeof(array)/sizeof(array[0]);
   printArray(array, length);
-  // mergeSort(array, length);
   heapSort(array, length);
   printArray(array, length);
   return 0;
