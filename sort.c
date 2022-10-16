9d6bf518b7546be704cc0215d5fd13cbfb5fd511
Andrei Eftimie <andrei@eftimie.com>
2022-10-16 10:09:25 +0300

    Add heapSort

--- sort.c
+++ sort.c
@@ -84,13 +84,55 @@ int mergeSort(int *arr, int length) {
   return 0;
 }
 
+int heapSort(int *arr, int length) {
+  for (int i = length / 2 - 1; i >= 0; i--) {
+    int j = i;
+    while (j * 2 + 1 < length) {
+      int k = j * 2 + 1;
+      if (k + 1 < length && arr[k] < arr[k + 1]) {
+        k++;
+      }
+      if (arr[j] < arr[k]) {
+        int temp = arr[j];
+        arr[j] = arr[k];
+        arr[k] = temp;
+        j = k;
+      } else {
+        break;
+      }
+    }
+  }
+  for (int i = length - 1; i > 0; i--) {
+    int temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    int j = 0;
+    while (j * 2 + 1 < i) {
+      int k = j * 2 + 1;
+      if (k + 1 < i && arr[k] < arr[k + 1]) {
+        k++;
+      }
+      if (arr[j] < arr[k]) {
+        int temp = arr[j];
+        arr[j] = arr[k];
+        arr[k] = temp;
+        j = k;
+      } else {
+        break;
+      }
+    }
+  }
+  return 0;
+}
+
 int main() {
   int array[] = {6, 4, 2, 8, 1, 9, 3, 5, 7, 0};
   int length = sizeof(array)/sizeof(array[0]);
   printArray(array, length);
   // insertSort(array, length);
   // divideEtImpoeraSort(array, length);
-  mergeSort(array, length);
+  // mergeSort(array, length);
+  heapSort(array, length);
   printArray(array, length);
   return 0;
 }
