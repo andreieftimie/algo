b363e78d0d163f48f67893a60844820e2f0f0fec
Andrei Eftimie <andrei@eftimie.com>
2022-10-16 11:03:23 +0300

    Move insertSort in separate file

--- sort.c
+++ sort.c
@@ -9,19 +9,6 @@ int printArray(int *arr, int length) {
   return 0;
 }
 
-int insertSort(int *arr, int length) {
-  for (int i = 1; i < length; i++) {
-    int j = i - 1;
-    int temp = arr[i];
-    while (j >= 0 && arr[j] > temp) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = temp;
-  }
-  return 0;
-}
-
 int mergeSort(int *arr, int length) {
   if (length <= 1) {
     return 0;
@@ -98,7 +85,6 @@ int main() {
   int array[] = {6, 4, 2, 8, 1, 9, 3, 5, 7, 0};
   int length = sizeof(array)/sizeof(array[0]);
   printArray(array, length);
-  // insertSort(array, length);
   // mergeSort(array, length);
   heapSort(array, length);
   printArray(array, length);
